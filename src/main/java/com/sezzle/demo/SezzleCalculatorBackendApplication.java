e02c7dc112d29f4b22f6a9e14b8b4ab0adbb28e1
UmeshMaharshi30 <umeshmaharshi@gmail.com>
2020-03-06 17:21:48 -0600

    Intitial commit

---
+++ src/main/java/com/sezzle/demo/SezzleCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sezzle.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SezzleCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SezzleCalculatorBackendApplication.class, args);
+	}
+
+}
